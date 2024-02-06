5f89b4c19a3b89b8ce73b42e6db2e826fb6e3446
Guillaume Mantopoulos <3082385+mrguiman@users.noreply.github.com>
2024-02-06 22:09:06 +0100

    work on home page layout

---
+++ static/index.js
@@ -0,0 +1,6 @@
+document.getElementById("open-mobile-nav").addEventListener("click", () => {
+    document.getElementById("nav-mobile").classList.add("opened");
+});
+document.getElementById("close-mobile-nav").addEventListener("click", () => {
+    document.getElementById("nav-mobile").classList.remove("opened");
+});
