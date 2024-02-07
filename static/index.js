1199fb2c67ba6aec08894eb5fa09b4300f058b85
Guillaume Mantopoulos <3082385+mrguiman@users.noreply.github.com>
2024-02-07 15:05:44 +0100

    refactor: simplify responsive navigation

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
-document.getElementById("open-mobile-nav").addEventListener("click", () => {
-    document.getElementById("nav-mobile").classList.add("opened");
-});
-document.getElementById("close-mobile-nav").addEventListener("click", () => {
-    document.getElementById("nav-mobile").classList.remove("opened");
+document.getElementById("mobile-menu").addEventListener("click", (e) => {
+     if(e.target.tagName=="A"){
+         let menuToggle = document.getElementById("mobile-menu-toggle");
+         menuToggle.checked = !menuToggle.checked;
+     }
 });
